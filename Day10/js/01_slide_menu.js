fb7ed064ab8d0bcb43c8582ea985afe292f2294a
NAMGUNGSEULAH <tmfdk7224@naver.com>
2021-12-17 12:54:34 +0900

    9일차

--- Day10/js/01_slide_menu.js
+++ Day10/js/01_slide_menu.js
@@ -3,6 +3,11 @@
 // jQuery    = $(function() {})
 // js        =     
 
+
+// 슬라이드 바 on/off
+let slideOn = 'off'
+let animateTime = 1500
+
 $(function() {
 
     // 메인 메뉴 = 마우스 클릭이벤트
@@ -11,44 +16,55 @@ $(function() {
     //    $(this).children('.submenu').stop().slideToggle(1000)
     
      // 모든 메뉴 슬라이드
-       $('.submenu').stop().slideToggle(1000)
+       $('.submenu').stop().slideToggle(animateTime)
         
     })
 
     // 햄버거 메뉴 클릭 이벤트
-    let slideOn = 'off'
     $('.slide-open').on('click', () => {
 
+      // 햄버거 메뉴 클릭 on
       if(slideOn == 'off'){
-        // $('.slide').animate({right:0}, 1000)
-        $('.slide').animate({left:0}, 2000)
-        // 햄버거 버튼 에니메이션
-        $('.slide-open').children('span').addClass('on')
-
-        $('.bg').css('display', 'block')
-
-        slideOn = 'on'
-      } else {
-        // $('.slide').animate({right:'-100%'}, 1000)
-        $('.slide').animate({left:'-100%'}, 2000)
-        // 햄버거 버튼 에니메이션
-        $('.slide-open').children('span').removeClass('on')
-
-        $('.bg').css('display', 'none')
-
-        slideOn = 'off'
+        slideBarOpen()
+      } 
+      // 햄버거 메뉴 클릭 off
+      else {
+        slideBarClose()
       }
     })
 
-    // 
-    $('.bg').on('click', () => {
-      $('.slide').animate({left:'-100%'}, 2000)
-      // 햄버거 버튼 에니메이션
-      $('.slide-open').children('span').removeClass('on')
-
-      $('.bg').css('display', 'none')
-
-      slideOn = 'off'
+    //  슬라이드 바 바깥 + x버튼 클릭시, 슬라이드 닫힘
+    $('.bg, .cancel').on('click', () => {
+        slideBarClose()
     })
 
 })
+
+// 슬라이드 바 열림
+function slideBarOpen() {
+  // $('.slide').animate({right:0}, 1000)
+  // 슬라이드 열림 애니메이션
+  $('.slide').animate({left:0}, animateTime)
+  // 햄버거 버튼 에니메이션
+  $('.slide-open').children('span').addClass('on')
+
+  // 슬라이드 바 바깥영역 - fade
+  // $('.bg').css('display', 'block')
+  $('.bg').fadeIn(animateTime)
+
+  slideOn = 'on'
+}
+
+// 슬라이드바 닫힘
+function slideBarClose() {
+  // 슬라이드 ekegla 애니메이션
+  $('.slide').animate({left:'-400px'}, animateTime)
+      // 햄버거 버튼 에니메이션
+      $('.slide-open').children('span').removeClass('on')
+
+      // 슬라이드 바 바깥영역 -fade
+      // $('.bg').css('display', 'none')
+      $('.bg').fadeOut(animateTime)
+
+      slideOn = 'off'
+}
